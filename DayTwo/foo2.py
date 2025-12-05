b77484dcf789f076ce9028e1bfd865b81b82f6a7
Sy0rin <151562060+Sy0rin@users.noreply.github.com>
2025-12-05 14:50:13 +0000

    crazy einstein moment fix

--- DayTwo/foo2.py
+++ DayTwo/foo2.py
@@ -9,11 +9,20 @@ try:
         for i in result:
             for j in range (i[0], i[1] + 1):
                 num = str(j)
-                way = len(num)//2
-                first_half = num[:way]
-                second_half = num[way:]
-                if first_half == second_half:
-                    total += j
+                curr = 0
+                pattern = []
+                checked = []
+                for i in range(len(num)):
+                    if num[i] == pattern[curr]:
+                        checked.append(num[i])
+                        curr += 1
+                    elif curr == 0:
+                        pattern.append(num[i])
+                    else:
+                        for k in checked:
+                            pattern.append(k)
+                        curr = 0
+                        i-=1      
         print(total)
 except FileNotFoundError:
     print("Error: The file 'your_file.txt' was not found.")
