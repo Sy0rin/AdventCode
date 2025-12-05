2b32db3004103917cc48cb13f7810ebb33d671df
Sy0rin <151562060+Sy0rin@users.noreply.github.com>
2025-12-05 21:40:07 +0000

    finished, soft finish as time is long exe

--- DayTwo/foo2.py
+++ DayTwo/foo2.py
@@ -3,28 +3,47 @@ import re
 try:
     with open('test.txt', 'r') as f:
         s = f.read()
-        flag = 0
         result = [list(map(int, part.split("-"))) for part in s.split(",")]
         total = 0;
         for i in result:
             for j in range (i[0], i[1] + 1):
+                # print("Checking number: ", j)
                 num = str(j)
-                curr = 0
+                curr, temp, flag, p, q = 0,0,0,0,0
                 pattern = []
                 checked = []
-                for i in range(len(num)):
-                    if num[i] == pattern[curr]:
-                        checked.append(num[i])
+                while q < len(num):
+                    # print("Pattern: ", pattern, " Checked: ", checked)
+                    if not pattern:
+                        pattern.append(num[q])
+                    elif num[q] == pattern[curr]:
+                        checked.append(num[q])
                         curr += 1
-                    elif curr == 0:
-                        pattern.append(num[i])
+                        if curr > len(pattern) - 1:
+                            curr = 0
+                    elif curr == 0 and q != len(num) and 0 == len(checked):
+                        pattern.append(num[q])
                     else:
                         for k in checked:
+                            # print("Adding ", k, " to pattern")
                             pattern.append(k)
+                        checked = []
                         curr = 0
-                        i-=1      
+                        q-=1
+                    q+=1
+                while p != len(num):
+                    if temp == len(pattern):
+                        temp = 0
+                    elif pattern[temp] == num[p]:
+                        p += 1
+                        temp += 1
+                        if p == len(num) and temp == len(pattern) and len(pattern) < len(num):
+                            flag = 1
+                    else:
+                        break
+                if flag:
+                    # print("Success ", j, " with pattern ", pattern)
+                    total += j
         print(total)
 except FileNotFoundError:
     print("Error: The file 'your_file.txt' was not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
